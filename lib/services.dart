20944b16537fb84aa845e659d51b9e1784a5a230
Mahmud Ahsan <mahmud@thinkdiff.net>
2019-10-22 11:20:49 +0800

    unnessary code removed

--- lib/services.dart
+++ lib/services.dart
@@ -2,23 +2,8 @@
  * Created by Mahmud Ahsan
  * https://github.com/mahmudahsan
  */
-import "package:flutter/material.dart";
 import "package:flutter_firebase_vote/models/vote.dart";
 import 'package:uuid/uuid.dart';
-
-Vote createMockVote() {
-  final vote = Vote(
-    voteId: Uuid().v4(),
-    voteTitle: 'Best Mobile Frameworks?',
-    options: [
-      {'Flutter': 0},
-      {'React Native': 0},
-      {'Xamarin': 0},
-    ],
-  );
-
-  return vote;
-}
 
 List<Vote> getVoteList() {
   // Mock Data
